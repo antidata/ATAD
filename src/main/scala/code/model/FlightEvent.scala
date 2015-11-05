3322bdcf6277ad440d985964b4b4349928e51845
antidata <antidata.05@gmail.com>
2015-11-05 00:49:10 -0600

    adding function to return flights to browser

--- src/main/scala/code/model/FlightEvent.scala
+++ src/main/scala/code/model/FlightEvent.scala
@@ -1,9 +1,11 @@
 package code.model
 
+import net.liftweb.json.{CustomSerializer, DefaultFormats, Extraction}
+import net.liftweb.json.JsonAST.{JNull, JInt, JString, JValue}
 import net.liftweb.mongodb.record.field.{MongoCaseClassField, DateField, JObjectField, ObjectIdPk}
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
 import net.liftweb.record.field.{DoubleField, StringField, DateTimeField}
-import com.foursquare.rogue.Rogue._
+import com.foursquare.rogue.LiftRogue._
 import com.foursquare.rogue.LatLong
 
 class FlightEvent private() extends MongoRecord[FlightEvent] with ObjectIdPk[FlightEvent] {
@@ -23,5 +25,52 @@ object FlightEvent extends FlightEvent with MongoMetaRecord[FlightEvent] {
   createIndex(id.name -> 1)
   createIndex(time.name -> 1)
   createIndex(loc.name -> "2d")
+  override implicit val formats = DefaultFormats + DoubleSerializer
+  case class FoldRightHelp(flights: Map[String, List[EventJson]], id: Int, lastTimestamp: Double)
+
+  def getFlightNumber(flight: String): JValue = {
+    val events = this.where(_.modelId eqs flight).fetch() map { event =>
+      EventJson(event.timestamp.get, event.modelId.get, event.anomalyScore.get, event.loc.get.lat, event.loc.get.long)
+    }
+    Extraction.decompose(
+      Flights(
+        events.foldLeft(FoldRightHelp(Map[String, List[EventJson]](), 0, 0D))((helper, event) => {
+          if((event.timestamp - helper.lastTimestamp).abs > 14400000D) { // 4 hrs
+            // New flight
+            FoldRightHelp(
+              helper.flights + (s"${helper.id + 1}" -> List(event)),
+              helper.id + 1,
+              event.timestamp)
+          } else {
+            FoldRightHelp(
+              helper.flights.updated(helper.id.toString, event :: helper.flights(helper.id.toString)),
+              helper.id,
+              event.timestamp)
+          }
+        }).flights.map(fidEvents => FlightEvents(fidEvents._1, fidEvents._2)).toList
+      )
+    )
+  }
 }
 
+case class Flights(flights: List[FlightEvents])
+
+case class FlightEvents(flightID: String, path: List[EventJson])
+
+case class EventJson(
+  timestamp: Double,
+  modelId: String,
+  anomalyScore: Double,
+  latitude: Double,
+  longitude: Double
+)
+
+case object DoubleSerializer extends CustomSerializer[Double](format => (
+  {
+    case JString(s) => s.toDouble
+    case JInt(n) => n.toDouble
+  },
+  {
+    case d: Double => JString(f"$d%15.4f")
+  }
+))
