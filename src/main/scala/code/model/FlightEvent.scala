e10be69458811074e7d46b943a3d31f8a40f5736
antidata <antidata.05@gmail.com>
2015-11-07 22:21:09 -0600

    Adding Realtime field

--- src/main/scala/code/model/FlightEvent.scala
+++ src/main/scala/code/model/FlightEvent.scala
@@ -4,11 +4,9 @@ import net.liftweb.json.{CustomSerializer, DefaultFormats, Extraction}
 import net.liftweb.json.JsonAST._
 import net.liftweb.mongodb.record.field.{MongoCaseClassField, DateField, JObjectField, ObjectIdPk}
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
-import net.liftweb.record.field.{DoubleField, StringField, DateTimeField}
+import net.liftweb.record.field.{BooleanField, DoubleField, StringField}
 import com.foursquare.rogue.LiftRogue._
 import com.foursquare.rogue.LatLong
-
-import scala.math.BigInt
 
 class FlightEvent private() extends MongoRecord[FlightEvent] with ObjectIdPk[FlightEvent] {
   override def meta = FlightEvent
@@ -19,6 +17,7 @@ class FlightEvent private() extends MongoRecord[FlightEvent] with ObjectIdPk[Fli
   object anomalyScore extends DoubleField(this)
   object flightData extends JObjectField(this)
   object loc extends MongoCaseClassField[FlightEvent, LatLong](this)
+  object realTime extends BooleanField(this)
 }
 
 object FlightEvent extends FlightEvent with MongoMetaRecord[FlightEvent] {
@@ -30,28 +29,36 @@ object FlightEvent extends FlightEvent with MongoMetaRecord[FlightEvent] {
   override implicit val formats = DefaultFormats + DoubleSerializer
   case class FoldRightHelp(flights: Map[Int, List[EventJson]], id: Int, lastTimestamp: Double)
 
-  def getFlightNumber(flight: String): JValue = {
-    val events = this.where(_.modelId eqs flight).fetch() map { event =>
+  def getFlightNumber(flight: String, realtime: Boolean = false): Flights = {
+    val initQuery = this.where(_.modelId eqs flight)
+    val finalQuery = if(realtime) initQuery.and(_.realTime eqs realtime) else initQuery
+    val events = finalQuery.fetch() map { event =>
       EventJson(event.timestamp.get, event.modelId.get, event.anomalyScore.get, event.loc.get.lat, event.loc.get.long)
     }
-    Extraction.decompose(
-      Flights(
-        events.foldLeft(FoldRightHelp(Map[Int, List[EventJson]](), 0, 0D))((helper, event) => {
-          if((event.timestamp - helper.lastTimestamp).abs > 14400000D) { // 4 hrs
-            // New flight
-            FoldRightHelp(
-              helper.flights + (helper.id + 1 -> List(event)),
-              helper.id + 1,
-              event.timestamp)
-          } else {
-            FoldRightHelp(
-              helper.flights.updated(helper.id, event :: helper.flights(helper.id)),
-              helper.id,
-              event.timestamp)
-          }
-        }).flights.filter(_._2.size > 500).map(fidEvents => FlightEvents(fidEvents._1, fidEvents._2)).toList
-      )
+    Flights(
+      events.foldLeft(FoldRightHelp(Map[Int, List[EventJson]](), 0, 0D))((helper, event) => {
+        if((event.timestamp - helper.lastTimestamp).abs > 14400000D) { // 4 hrs
+          // New flight
+          FoldRightHelp(
+            helper.flights + (helper.id + 1 -> List(event)),
+            helper.id + 1,
+            event.timestamp)
+        } else {
+          FoldRightHelp(
+            helper.flights.updated(helper.id, event :: helper.flights(helper.id)),
+            helper.id,
+            event.timestamp)
+        }
+      }).flights.filter(t => realtime || t._2.size > 500).map(fidEvents => FlightEvents(fidEvents._1, fidEvents._2)).toList
     )
+  }
+
+  implicit def flights2Jvalue(flights: Flights): JValue = {
+    Extraction.decompose(flights)
+  }
+
+  implicit def event2Jvalue(event: EventJson): JValue = {
+    Extraction.decompose(event)
   }
 }
 
