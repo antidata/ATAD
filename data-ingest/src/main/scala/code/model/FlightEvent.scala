95ddc425f77854315545f3f682833120f0ca2ad3
antidata <antidata.05@gmail.com>
2015-11-07 22:35:04 -0600

    Adding realtime

--- data-ingest/src/main/scala/code/model/FlightEvent.scala
+++ data-ingest/src/main/scala/code/model/FlightEvent.scala
@@ -2,7 +2,7 @@ package code.model
 
 import net.liftweb.mongodb.record.field.{MongoCaseClassField, DateField, JObjectField, ObjectIdPk}
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
-import net.liftweb.record.field.{DoubleField, StringField, DateTimeField}
+import net.liftweb.record.field.{BooleanField, DoubleField, StringField, DateTimeField}
 import com.foursquare.rogue.Rogue._
 import com.foursquare.rogue.LatLong
 
@@ -15,6 +15,7 @@ class FlightEvent private() extends MongoRecord[FlightEvent] with ObjectIdPk[Fli
   object anomalyScore extends DoubleField(this)
   object flightData extends JObjectField(this)
   object loc extends MongoCaseClassField[FlightEvent, LatLong](this)
+  object realTime extends BooleanField(this)
 }
 
 object FlightEvent extends FlightEvent with MongoMetaRecord[FlightEvent] {
